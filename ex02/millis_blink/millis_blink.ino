9b31da1c51b6ee7c6962d50694c4976706d31f82
翟睿程 <2244646589@qq.com>
2026-06-18 21:10:16 +0800

    完成作业2：用millis()实现LED 1Hz闪烁

---
+++ ex02/millis_blink/millis_blink.ino
@@ -0,0 +1,21 @@
+// ex02/millis_blink.ino
+// 使用 millis() 实现 LED 以 1Hz 频率闪烁（亮0.5秒，灭0.5秒）
+
+const int ledPin = 2;          // 改成你实际连接的引脚
+unsigned long previousMillis = 0;
+const long interval = 500;
+bool ledState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
+}
